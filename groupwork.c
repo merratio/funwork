3bad1c9bd72dcc1d67b8c88a3bfca82ccb7ebad4
JUNO2434 <112677792+JUNO2434@users.noreply.github.com>
2023-03-29 23:52:40 -0500

    commented

--- groupwork.c
+++ groupwork.c
@@ -76,36 +76,51 @@ int main(void)
     printf("Detailing/InteriorShampooing\t$7500.00\n");
 
 
+    // The customerInformation function collects information about a new customer and returns it
     customerInformation();
+    
+    // The wash function performs the washing service for a customer
     wash();
+    
+    // The searchReturningCustomers function search a database for returning customers using their license plate number
     searchReturningCustomers();
+    
+    // The displaySalesReport function calculates and displays the total sales from each service, as well as overall total sales
     displaySalesReport();
-
+    
 
 }
 
 // Function to add new customers to the car wash
-void customerInformation(){
+// The customerInformation function collects information about a new customer
+void customerInformation() {
+    // Declare variables for storing tip percentage, cost, total cost and an integer value
     int tip, tipPercentage, integer;
     float cost, totalCost = 0;
+
+    // Declare a variable of type CUS and a pointer to the same type
     CUS coustomer;
     CUS *coustomerptr;
 
+    // Set pointer to point to the address of coustomer
     coustomerptr = &coustomer;
 
-
+    // Generate seed for random number generator using current time
     srand(time(NULL));
 
+    // Prompt user to enter an integer to start program
     printf("Enter any integer to start program: ");
-    scanf("%d",&integer);
+    scanf("%d", &integer);
 
-    while(integer !=-1){
-
-        if(numberofCustomers == 23){
+    // Continue running program as long as integer is not -1
+    while(integer != -1) {
+        // Check if the car wash is full
+        if(numberofCustomers == 23) {
             printf("Car wash is full");
             break;
         }
-        else{
+        else {
+
             // Prompt user for first name and store in variable 'fname'
             printf("Enter first name: ");
             scanf("%s",coustomerptr->cusFirstName);
@@ -123,64 +138,71 @@ void customerInformation(){
             printf("Enter coustomers plate number: ");
             scanf("%s",coustomerptr->lisPlatNum);
 
-            printf("1.WashandVacuum\n");
-            printf("2.EngineWash\n");
-            printf("3.Polishing\n");
-            printf("4.Buffing\n");
-            printf("5.RoofCleaning\n");
-            printf("6.Detailing/InteriorShampooing\n");
-
-            do{
-                printf("what are the service(s) needed by customers(enter -1 when finished choosing services): ");
-                scanf("%d",&coustomerptr->servicesRequired);
-
-                switch (coustomerptr->servicesRequired)
-                {
-                case 1:
-                    cost = 2500;
-                    washCount++;
-                    break;
-                case 2:
-                    cost = 2000;
-                    engineCount++;
-                    break;
-                case 3:
-                    cost = 3500;
-                    polishCount++;
-                    break;
-                case 4:
-                    cost = 5500;
-                    buffCount++;
-                    break;
-                case 5:
-                    cost = 3200;
-                    roofCount++;
-                    break;;
-                case 6:
-                    cost = 7500;
-                    detailCount++;
-                    break;
-                default:
-                    printf("Invalid service.\n");
-                    cost = 0;
-                    break;
-                }
-
-                totalCost += cost;
-
-            }while(coustomerptr->servicesRequired != -1);
-
-
-            printf("What is the preffered mode of payment\n1. Cash\n2. Credit\n");
-            scanf("%d",&coustomerptr->paymentMethod);
-
-            if(coustomerptr->paymentMethod == 1){
-                printf("payment method: Cash\n");
-            }
-            else if(coustomerptr->paymentMethod == 2){
-                printf("payment method: Credit\n");
-                totalCost = (totalCost * 0.03) + totalCost;
-            }
+           // Prints out the available services for the user to choose from.
+           printf("1.WashandVacuum\n");
+           printf("2.EngineWash\n");
+           printf("3.Polishing\n");
+           printf("4.Buffing\n");
+           printf("5.RoofCleaning\n");
+           printf("6.Detailing/InteriorShampooing\n");
+           
+           // Prompt the user to input their desired service and store it in coustomerptr->servicesRequired.
+           do{
+               printf("what are the service(s) needed by customers(enter -1 when finished choosing services): ");
+               scanf("%d",&coustomerptr->servicesRequired);
+           
+               // Use a switch statement to match the selected service with its corresponding cost and increase the count of each service.
+               switch (coustomerptr->servicesRequired)
+               {
+                   case 1:
+                       cost = 2500;
+                       washCount++;
+                       break;
+                   case 2:
+                       cost = 2000;
+                       engineCount++;
+                       break;
+                   case 3:
+                       cost = 3500;
+                       polishCount++;
+                       break;
+                   case 4:
+                       cost = 5500;
+                       buffCount++;
+                       break;
+                   case 5:
+                       cost = 3200;
+                       roofCount++;
+                       break;;
+                   case 6:
+                       cost = 7500;
+                       detailCount++;
+                       break;
+                   default:
+                       printf("Invalid service.\n");
+                       cost = 0;
+                       break;
+               }
+           
+               // Calculate the total cost by adding up the costs of all the selected services.
+               totalCost += cost;
+           
+           }while(coustomerptr->servicesRequired != -1);
+           
+           // Prompt the user to input their preferred mode of payment and store it in coustomerptr->paymentMethod.
+           printf("What is the preferred mode of payment\n1. Cash\n2. Credit\n");
+           scanf("%d",&coustomerptr->paymentMethod);
+           
+           // If payment method is cash, print "payment method: Cash".
+           if(coustomerptr->paymentMethod == 1){
+               printf("payment method: Cash\n");
+           }
+           // If payment method is credit, print "payment method: Credit" and calculate the total cost by adding 3% to the original totalCost.
+           else if(coustomerptr->paymentMethod == 2){
+               printf("payment method: Credit\n");
+               totalCost = (totalCost * 0.03) + totalCost;
+           }
+           
         
 
             // Open a file named 'carwash.txt' for writing customer information
@@ -393,17 +415,35 @@ void wash(){
 }
 
 
+// This function displays a sales report with the amount earned from various services and other information.
 void displaySalesReport(){
+    // Displays the heading for the service totals
     printf("\nservice totals\n");
+
+    // Displays the amount earned from wash and vacuum services
     printf("amount made from wash and vaccum: %.2f\n", washCount * 2500);
+
+    // Displays the amount earned from engine wash services
     printf("amount made from engine wash: %.2f\n", engineCount * 2000);
+
+    // Displays the amount earned from polishing services
     printf("amount made from polishing: %.2f\n", polishCount * 3500);
+
+    // Displays the amount earned from buffing services
     printf("amount made from buffing %.2f\n", buffCount * 5500);
+
+    // Displays the amount earned from roof cleaning services
     printf("amount made from roof cleaning: %.2f\n", roofCount * 3200);
+
+    // Displays the amount earned from detailing/interior shampooing services
     printf("amount made from detailing/interior shampooing: %.2f\n", detailCount * 7500);
 
+    // Displays the total number of customers served
     printf("Total number of customers: %d\n",totalNumberofCustomers);
+
+    // Displays the total amount earned
     printf("Total amount earned: %.2f\n",tcost);
-    printf("Total amount earned in tips: %.2f\n",totalTips);
-  
+
+    // Displays the total amount earned in tips
+    printf("Total amount earned in tips: %.2f\n",totalTips);  
 }
